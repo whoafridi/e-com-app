ca7aa9ce457ff7d91e3bd1878cbaf2ed0fbfcfb8
whoafridi <afridirk10@gmail.com>
2024-02-07 15:15:57 +0600

    initial setup with tailwind, shadcn ui

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <p className='my-5'>
+      <Button variant="destructive" value="LOL">lol</Button>      
+    </p>
+  )
+}
