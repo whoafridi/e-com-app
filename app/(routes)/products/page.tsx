c067570076ee4be86d2c111a2186f201bdd47448
whoafridi <afridirk10@gmail.com>
2024-02-08 23:24:28 +0600

    create products page

---
+++ app/(routes)/products/page.tsx
@@ -0,0 +1,12 @@
+import Products from "@/components/Products/Products";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Products />
+    </>
+  );
+};
+
+export default page;
