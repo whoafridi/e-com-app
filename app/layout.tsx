74b819902af33d02bd2758c92aade11269294c36
whoafridi <afridirk10@gmail.com>
2024-05-08 14:44:32 +0600

    create rootlayout

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import Footer from "@/components/Footer";
+import Nav from "@/components/Nav/Nav";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "e-com app",
+  description:
+    "e-commerce app aims to sell products and make something all together.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
