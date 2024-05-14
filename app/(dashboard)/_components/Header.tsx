68300dda97ec42012c7bf763a113c6b872ab52a0
whoafridi <afridirk10@gmail.com>
2024-05-14 12:01:36 +0600

    small improvements in header

--- app/(dashboard)/_components/Header.tsx
+++ app/(dashboard)/_components/Header.tsx
@@ -16,7 +16,7 @@ import { Button } from "@/components/ui/button";
 const Header = () => {
   return (
     <header className="relative z-10 bg-white border-b border-gray border-solid py-5 px-8 pr-8">
-      <div className="flex justify-between">
+      <div className="flex justify-between gap-5">
         <div className="flex items-center space-x-6 lg:space-x-0">
           <MobileAsidebar />
           <div className="relative ml-auto flex-1 md:grow-0">
@@ -28,34 +28,7 @@ const Header = () => {
             />
           </div>
         </div>
-        <div className="flex items-center justify-end space-x-2">
-          <div className="md:hidden">
-            <button className="relative w-[40px] h-[40px] leading-[40px] rounded-md text-textBody border border-gray hover:bg-themeLight hover:text-theme hover:border-themeLight">
-              <svg
-                className="-translate-y-[2px]"
-                width="16"
-                height="16"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M9 17C13.4183 17 17 13.4183 17 9C17 4.58172 13.4183 1 9 1C4.58172 1 1 4.58172 1 9C1 13.4183 4.58172 17 9 17Z"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-                <path
-                  d="M18.9999 19L14.6499 14.65"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-              </svg>
-            </button>
-          </div>
+        <div className="flex items-center justify-end space-x-2">         
           <div className="relative">
             <Button
               variant="outline"
