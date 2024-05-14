741b5188ba93bdfde55ba2c05cc02e072a8f499b
whoafridi <afridirk10@gmail.com>
2024-05-14 12:02:31 +0600

    feat: add icon in search input

--- components/ui/filterInput.tsx
+++ components/ui/filterInput.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import { Input } from './input';
+import React from "react";
+import { Input } from "./input";
+import { Search } from "lucide-react";
 
 type IFilterProps = {
-    table: any;
-    filterItem?: string;
-    filterPlaceHolder?: string;
+  table: any;
+  filterItem?: string;
+  filterPlaceHolder?: string;
 };
 
-const FilterInput = ({ table, filterItem, filterPlaceHolder }: IFilterProps) => {
-    return (
-        <div>
-            <Input
-                placeholder={filterPlaceHolder}
-                value={(table.getColumn(filterItem)?.getFilterValue() as string) ?? ""}
-                onChange={(event) =>
-                    table.getColumn(filterItem)?.setFilterValue(event.target.value)
-                }
-                className="max-w-sm"
-            />
-        </div>
-    );
+const FilterInput = ({
+  table,
+  filterItem,
+  filterPlaceHolder,
+}: IFilterProps) => {
+  return (
+    <div className="relative md:grow-0">
+      <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+      <Input
+        placeholder={filterPlaceHolder}
+        value={(table.getColumn(filterItem)?.getFilterValue() as string) ?? ""}
+        onChange={(event) =>
+          table.getColumn(filterItem)?.setFilterValue(event.target.value)
+        }
+        className="max-w-sm w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+      />
+    </div>
+  );
 };
 
 export default FilterInput;
